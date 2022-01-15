88ec8e2c288909d3f033a4f9aaa1204094285e2f
ubairnoor <ubairnoor5@gmail.com>
2022-01-15 16:08:26 +0530

    add container [ import connect and import action adata , connect maptoprp and state and send to home component]

--- src/container/HomeContainer.js
+++ src/container/HomeContainer.js
@@ -1,3 +1,18 @@
+import {connect}  from 'react-redux'
+import { addToCart } from '../service/actions/actions'
+
+
 import Home from '../component/Home'
 
-export default Home
+
+const mapDispatchToProps = dispatch =>({
+    addToCartHandler:data =>dispatch(addToCart(data)) 
+
+})
+const mapDispatchToState = state =>({
+
+})
+
+export default connect (mapDispatchToState,mapDispatchToProps)(Home)
+
+// export default Home
